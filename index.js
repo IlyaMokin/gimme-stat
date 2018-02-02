1c880f08354035e29f7752cc321f42dd833e41a5
Ilya Mokin <ilya.mokin@primaryintelcom.onmicrosoft.com>
2018-02-02 21:49:47 +0300

    stat preparing

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //git log --no-merges --pretty=short --stat --since 2017/09/14 --until 2018/01/01
 //https://regex101.com/r/HjC7th/1
+//https://www.npmjs.com/package/console-progress
+//https://www.npmjs.com/package/msee
+//https://www.npmjs.com/package/json2md
+//https://stackoverflow.com/questions/11509830/how-to-add-color-to-githubs-readme-md-file - colors
 const git = require('git-cmd');
+const ignoreList = [
+    'node_modules',
+    'package.lock',
+    'package.json'
+]
 
 function getStat(since, until) {
     let cmd = git([
