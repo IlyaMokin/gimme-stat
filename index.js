f9af2f954aefd6177dc8d9a14ab2e4eb58e55dd3
maksim.sarvas <ladamashina@gmail.com>
2018-02-19 13:55:33 +0300

    add await to checkExistence

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require.extensions['.ejs'] = (module, filename) => {module.exports = fs.readFile
     if (config.init) {
         let configPath = `${process.cwd()}/gimme.config.js`;
         const checkExistence = util.promisify(fs.stat);
-        checkExistence(configPath).then(()=> console.log('Fail: gimme.config.js already exists.') )
+        await checkExistence(configPath).then(()=> console.log('Fail: gimme.config.js already exists.') )
             .catch(()=>{ fs.createReadStream(`${__dirname}/default-config.js`).pipe(fs.createWriteStream(configPath));
             console.log(`Config file created at ${process.cwd()}/gimme.config.js`);});
         return;
