633f79d26b7141d69ec3f9dce808ab02ab6863ba
maksim.sarvas <ladamashina@gmail.com>
2018-06-07 19:00:09 +0300

    readme fix

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// function polymorph() {
-//     var len2func = [];
-//     for (var i = 0; i < arguments.length; i++)
-//         if (typeof (arguments[i]) == "function")
-//             len2func[arguments[i].length] = arguments[i];
-//     return  function () {
-//         return  len2func[arguments.length].apply(this, arguments);
-//     }
-// }
-
-// var gimmeStat = polymorph(
-//      function () {
-//         let stat = require('./stat');
-//         const config = require('./env');
-//         if (require.main !== module) {
-//             config.returnString = true;
-//             var result =  stat(config);
-//             return result;
-//         }
-//         if (require.main === module) {
-//             stat(config);
-//         }
-//     },
-//     function (object) {
-//         let stat = require('./stat');
-//         let config = require('./env');
-//         config = Object.assign(config, object);
-//         config.returnString = true;
-//         return stat(config);
-//     }
-// )
 var gimmeStat = {
     cmd: () => {
         let stat = require('./stat');
